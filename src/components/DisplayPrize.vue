d10c8e7cc5129461d60c45bb25d3283ab3747532
Veronica Elliott <Mr_Digital@Veronicas-MacBook-Air.local>
2017-09-27 22:18:45 -0400

    changed localhost routes

--- src/components/DisplayPrize.vue
+++ src/components/DisplayPrize.vue
@@ -46,13 +46,13 @@ export default {
 
     methods: {
         fetchPrizes() {
-            let uri = 'http://localhost:4000/prizes';
+            let uri = 'https://damp-forest-70063.herokuapp.com/prizes';
             this.axios.get(uri).then((response) => {
                 this.prizes = response.data;
             });
         },
         deletePrize(id) {
-            let uri = 'http://localhost:4000/prizes/delete/' + id;
+            let uri = 'https://damp-forest-70063.herokuapp.com/prizes/delete/' + id;
             this.prizes.splice(id, 1);
             this.axios.get(uri);
         }
